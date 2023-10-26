809e2a438d79e4b4de8cd77ae490f5af83eb8e26
kamafull <gavrilovglebm@gmail.com>
2023-10-26 18:22:43 +0300

    1

--- src/main/java/ru/digitalhabbits/homework4/HomeworkApplication.java
+++ src/main/java/ru/digitalhabbits/homework4/HomeworkApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 @SpringBootApplication
-public class HomeworkApplication
-        implements ApplicationRunner {
+@PropertySource("classpath:config/10-additional.properties")
+@PropertySource("classpath:config/20-additional.properties")
+public class HomeworkApplication implements ApplicationRunner {
     private static final Logger logger = getLogger(HomeworkApplication.class);
 
     @Value("${homework.message}")
@@ -23,6 +25,7 @@ public class HomeworkApplication
 
     @Override
     public void run(ApplicationArguments args) {
+        System.out.println(message);
         logger.info(message);
     }
 }
